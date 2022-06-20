405ed0b545256d2d4f1ad0e149d5052d68f93a5e
Jochem Smit <jhsmit@gmail.com>
2022-06-20 11:04:38 +0200

    update clean_types function

--- {{cookiecutter.yymmdd}}_{{cookiecutter.project_slug}}/hal/utils.py
+++ {{cookiecutter.yymmdd}}_{{cookiecutter.project_slug}}/hal/utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from typing import Any
+from collections import OrderedDict
 
 import numpy as np
 
@@ -9,10 +10,12 @@ def longpath(pth: Path) -> Path:
     return Path(r"//?/" + str(pth))
 
 
-def clean_dict(d: dict) -> Any:
-    """cleans up dictionary `d` for exporting as yaml
-    dictionary is modified in-place (!)
+def clean_types(d: Any) -> Any:
+    """cleans up nested dict/list/tuple/other `d` for exporting as yaml
 
+    Converts library specific types to python native types, including numpy dtypes,
+    OrderedDict, numpy arrays
+    
     # https://stackoverflow.com/questions/59605943/python-convert-types-in-deeply-nested-dictionary-or-array
 
     """
@@ -25,8 +28,17 @@ def clean_dict(d: dict) -> Any:
     if isinstance(d, np.ndarray):
         return d.tolist()
 
-    if isinstance(d, dict):
-        for k, v in d.items():
-            d.update({k: clean_dict(v)})
+    if isinstance(d, list):
+        return [clean_types(item) for item in d]
 
-    return d
+    if isinstance(d, tuple):
+        return tuple(clean_types(item) for item in d)
+
+    if isinstance(d, OrderedDict):
+        return clean_types(dict(d))
+
+    if isinstance(d, dict):
+        return {k: clean_types(v) for k, v in d.items()}
+
+    else:
+        return d
