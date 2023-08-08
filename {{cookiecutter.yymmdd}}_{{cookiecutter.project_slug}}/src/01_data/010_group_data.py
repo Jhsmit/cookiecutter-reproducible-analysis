ee60891c55e17ebdbeb2e8025d087d42ad23c945
Jochem Smit <jhsmit@gmail.com>
2023-08-08 17:06:36 +0200

    fixes

--- {{cookiecutter.yymmdd}}_{{cookiecutter.project_slug}}/src/01_data/010_group_data.py
+++ {{cookiecutter.yymmdd}}_{{cookiecutter.project_slug}}/src/01_data/010_group_data.py
@@ -1,13 +1,12 @@
-#%%
+# %%
 from hal.config import cfg
 from hal.repro import reproduce
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from hal.fileIO import dataframe_to_file
 
-#%%
+# %%
 
 
 # List additional dependencies to log
@@ -18,7 +17,5 @@ packages = [
 OUTPUT_PATH = reproduce(globals(), packages=packages)
 
 
-
-#%%
+# %%
 # Global variables
-
--- {{cookiecutter.yymmdd}}_{{cookiecutter.project_slug}}/src/01_data/020_preprocess_data.py
+++ {{cookiecutter.yymmdd}}_{{cookiecutter.project_slug}}/src/01_data/020_preprocess_data.py
@@ -1,13 +1,10 @@
-#%%
-from hal.config import cfg
+# %%
 from hal.repro import reproduce
-from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from hal.fileIO import dataframe_to_file
 
-#%%
+# %%
 
 
 # List additional dependencies to log
@@ -18,17 +15,15 @@ packages = [
 OUTPUT_PATH = reproduce(globals(), packages=packages)
 
 
-#%%
+# %%
 # Global variables
 
 NUM = 20
 OVERWRITE = True
 rng = np.random.RandomState()
 
-#%%
+# %%
 
 col_names = [f"col_{c}" for c in range(NUM)]
 data = rng.rand(30, NUM)
 df = pd.DataFrame(data, columns=col_names)
-
-dataframe_to_file(df, OUTPUT_PATH / "gen_data.csv", fmt="csv")
