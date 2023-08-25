a182669487e3bee22cb837e5d66533cac91d0149
Jochem Smit <jhsmit@gmail.com>
2023-08-25 15:57:04 +0200

    rpr dir as zipfile

--- {{cookiecutter.yymmdd}}_{{cookiecutter.project_slug}}/hal/repro.py
+++ {{cookiecutter.yymmdd}}_{{cookiecutter.project_slug}}/hal/repro.py
@@ -4,6 +4,7 @@ import sys
 from pathlib import Path
 from types import ModuleType
 from typing import Generator, Iterable, Optional
+import zipfile
 
 import watermark
 from hal.config import cfg
@@ -19,6 +20,67 @@ def gen_imports(globals_: dict) -> Generator:
                 yield val.__module__.split(".")[0]
             except AttributeError:
                 pass
+
+
+def zipdir(path: Path, zipf: zipfile.ZipFile):
+    for file_path in path.glob("**/*"):
+        if "__pycache__" in file_path.parts:
+            continue
+
+        if file_path.is_file():
+            zipf.write(file_path, file_path.relative_to(path.parent))
+
+
+def reproduce(
+    globals_: dict,
+    packages: Optional[Iterable[str]] = None,
+    **watermark_kwargs,
+) -> Path:
+    script_path = Path(globals_["__file__"])
+    relpath = script_path.relative_to(cfg.paths.src)
+    output_path = cfg.paths.output / relpath.parent / relpath.stem
+    output_path.mkdir(exist_ok=True, parents=True)
+
+    combined = set(packages or []) | set(gen_imports(globals_))
+
+    # Remove builtins
+    combined -= {sys.builtin_module_names}
+
+    # Remove standard library
+    stdlib_pth = sysconfig.get_python_lib(standard_lib=True)
+    stdlib = {p.stem.replace(".py", "") for p in Path(stdlib_pth).iterdir()}
+    combined -= stdlib
+
+    # Remove hal
+    combined -= {"hal", "builtins", "src"}
+
+    packages = ",".join(sorted(combined))
+    dave_kwargs = dict(
+        author="{{cookiecutter.author_name}}",
+        current_time=True,
+        current_date=True,
+        timezone=True,
+        updated=True,
+        python=True,
+        conda=True,
+        githash=True,
+        machine=True,
+    )
+
+    dave_kwargs.update(watermark_kwargs)
+    dave = watermark.watermark(globals_=globals_, packages=packages, **dave_kwargs)
+
+    with zipfile.ZipFile(
+        output_path / "_rpr.zip", "w", zipfile.ZIP_DEFLATED
+    ) as rpr_zip:
+        rpr_zip.write(script_path, script_path.name)
+        rpr_zip.writestr("watermark.txt", dave)
+
+        toolbox_dir = cfg.paths.src / "toolbox"
+        if toolbox_dir.exists():
+            zipdir(toolbox_dir, rpr_zip)
+
+    return output_path
 
 
 def reproduce(
